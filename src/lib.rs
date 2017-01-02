a7302e8581ec19224f46393d470aff8d39edff44
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-01-02 11:12:00 -0500

    Add PartialEq and PartialOrd to Angle trait

--- src/lib.rs
+++ src/lib.rs
@@ -87,7 +87,7 @@ pub trait IntoAngle<To>
 }
 
 /// Base functionality for all angle types.
-pub trait Angle: Clone + FromAngle<Self> {
+pub trait Angle: Clone + FromAngle<Self> + PartialEq + PartialOrd {
     /// Internal type storing the angle value.
     type Scalar: Float;
 

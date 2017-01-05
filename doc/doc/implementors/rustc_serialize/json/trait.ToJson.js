7b8d9faa6e404adcf80f124378fb51dc98afd355
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-01-04 20:22:29 -0500

    Update doc to 0.2.1

--- doc/doc/implementors/rustc_serialize/json/trait.ToJson.js
+++ doc/doc/implementors/rustc_serialize/json/trait.ToJson.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustc_serialize"] = [];implementors["num"] = [];
+implementors["rustc_serialize"] = [];implementors["num"] = [];implementors["num"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
